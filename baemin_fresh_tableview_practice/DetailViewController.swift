06fbe5b5707616fbd3dcbcd160bc98f6a497ff30
dain7672 <dain7672@gmail.com>
2017-07-19 21:52:39 +0900

    almost done with vertical scroll

---
+++ baemin_fresh_tableview_practice/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  baemin_fresh_tableview_practice
+//
+//  Created by woowabrothers on 2017. 7. 19..
+//  Copyright © 2017년 woowabrothers_dain. All rights reserved.
+//
+
+import Foundation
